9e6b5a1af98857282f43df4f7359bdc888cfbb7e
mmitusov <maksym.mitusov@gmail.com>
2022-07-15 19:45:01 +0300

    Testing localhost

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ require('dotenv').config() //
 const express = require('express')
 const sequelize = require('./db')
 const db_models = require('./db_models/db_models')
+const cors = require('cors')
 
 const app = express();
+app.use(cors())
+//app.use(express.json()); - Latest version of exressJS now comes with Body-Parser, so we don't need this!
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Working'})
+})
 
 const start = async() => {
     try {
@@ -17,4 +24,4 @@ const start = async() => {
 }
 start()
 
-app.listen(process.env.PORT || 5001, () => console.log(`Server is running on the port ${process.env.PORT}`))
+app.listen(process.env.PORT || 3002, () => console.log(`Server is running on the port ${process.env.PORT}`))
