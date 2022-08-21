3706e8cbdf438331570a7e8e2e74940011ce9a36
mmitusov <maksym.mitusov@gmail.com>
2022-08-21 23:49:18 +0300

    authMiddleware implementation

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,10 +1,18 @@
-//Здесь мы будем декодировать JWT токен при регистрации или логине и проверять его на валидность
-module.export = function (req, res, next) {
+//Здесь мы будем декодировать JWT токен при регистрации/логине и проверять его на валидность
+const jwt = require('jsonwebtoken') //Для разкодировки токена
+
+module.exports = function (req, res, next) {
     if (req.method === "OPTIONS") {
         next()
     }
     try {
-
+        const token = req.headers.authorization.split(' ')[1] //Токен зачастую помещают в 'headers.authorization'. И помещают его в виде 'тип_токена.сам_токен'. Поэтому отделяем их друг от друга и получаем сам токен по 1-му индексу
+        if (!token) {
+            return res.status(401).json({message: `Token is not found. You're not athorized`})
+        }
+        const decoded = jwt.verify(token, process.env.SECRET_KEY) //Далее мы должны разкодировать полученный токен и при помощи .verify проверить его на валидность
+        req.user = decoded //К реквесту (например в поле юзер) добавим данные которые мы вытащили. И во всех функциях этот юзер будет доступен
+        next() //С помощью next вызываем следующий в цепочке middleware
     } catch (err) {
         res.status(401).json({message: "Not authorized"})
     }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router(); 
 const userController = require('../controllers/UserController') 
-
+const authMiddleware = require('../middleware/authMiddleware') //Импортируем созданный middleware из 'authMiddleware' 
 
 router.post('/registration', userController.registration)
 router.post('/login', userController.login)
-router.get('/auth', userController.check)
+router.get('/auth', authMiddleware, userController.check) //И добывляем authMiddleware в get запрос вторым параметром, который будет проверять пользователя на авторизованость
 
 //This is how we've tested this path before - we've created our controllers paths
 //We've mooved our controller to a sepatate folder so we can keep current file clean and readble
